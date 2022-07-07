a9ed80426f7de658b999fcd3fd18c32af9aa71f9
syowaki <wakisaka@engolfer.jp>
2022-07-07 18:03:56 +0900

    Found a solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+//array, and the rest are numbers
 
+
+const removeFromArray = function(ogArray, a, b, c, d) {
+    const newArray = []
+    for(const number of ogArray) {
+        let found = false;
+        if(number === a) {
+            found=true;
+        }
+        if(number === b) {
+            found=true;
+        }
+        if(number === c) {
+            found=true;
+        }
+        if(number === d) {
+            found=true;
+        }
+        if(found === false) {
+            newArray.push(number)
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
