e717fa3adffeccaa0b56d3eab9a09cc2c39b5688
syowaki <wakisaka@engolfer.jp>
2022-07-07 11:57:45 +0900

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+let string;
+let num;
 
+const repeatString = function(string, num) {
+    let result = "";
+    if(num < 0) {
+        result = "ERROR"
+    }else {
+        for(i = 0; i < num; i++) {
+            result = string + result
+        }
+    }
+    return result;
 };
+
+repeatString("hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;
