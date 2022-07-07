5e08440eff42c0496e148958743e388c99f2ce69
syowaki <wakisaka@engolfer.jp>
2022-07-07 15:29:03 +0900

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+//get a variable called string
+//map out each letter of the string 
 
+const reverseString = function(string) {
+    let stringReverse = [];
+    const stringArray = string.split("");
+    const stringLength = stringArray.length
+    for(i = stringLength; i >= 0; i--) {
+        const letter = stringArray[i]
+        stringReverse.push(letter)
+    }
+    return stringReverse.join("")
 };
 
 // Do not edit below this line
