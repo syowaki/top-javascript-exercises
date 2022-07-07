db8b97734754ec4ca8c5013e11630c62d0f2f13a
syowaki <wakisaka@engolfer.jp>
2022-07-08 08:56:45 +0900

    Completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+//if statements
+//%4 = 0  true
+//%4 = 0 && %100 = 0   false
+//%4 = 0 && %400 = 0   true
+const leapYears = function(year) {
+    if(year % 400 === 0) {
+        return true;
+    }
+    else if(year % 100 === 0) {
+        return false;
+    }
+    else if(year % 4 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
