2b830f517890535cf66638dfaa43657fa361abae
syowaki <wakisaka@engolfer.jp>
2022-07-08 09:12:48 +0900

    Completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  if(typeof f !== "number") {
+    return "ERROR"
+  }
+  let celsius = (f-32)*5/9
+  let celsiusRounded = Math.round(celsius * 10) / 10
+  return celsiusRounded
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  if(typeof c !== "number") {
+    return "ERROR"
+  }
+  let farenheit = (c*9/5)+32
+  let farenheitRounded = Math.round(farenheit * 10) / 10
+  return farenheitRounded
 };
 
 // Do not edit below this line
