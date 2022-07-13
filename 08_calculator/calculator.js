5ed609a9c56ea1a08090d74f8419e37b2f860000
syowaki <wakisaka@engolfer.jp>
2022-07-13 10:31:58 +0900

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	let sumTotal = x.reduce(function(result, item) {
+    return result + item;
+  }, 0)
+  return sumTotal
 };
 
-const multiply = function() {
-
+const multiply = function(y) {
+  let multiplyTotal = y.reduce(function(result, item) {
+    return result * item
+  }, 1)
+  return multiplyTotal
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  let powerTotal = 1;
+  for(i = 0; i < y; i++) {
+    powerTotal *= x
+  }
+  return powerTotal
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let factorialTotal = 1
+	for(i = x; i > 0; i--) {
+    factorialTotal *= i
+  }
+  return factorialTotal;
 };
 
 // Do not edit below this line
