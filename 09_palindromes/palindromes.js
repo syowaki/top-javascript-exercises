0ed50d32d559e98c75ce052280b44b50fb538c0c
syowaki <wakisaka@engolfer.jp>
2022-07-13 11:34:28 +0900

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,41 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringLowerCase = string.toLowerCase();
+    let stringAsArray = stringLowerCase.split("");
+    let arrayNoPunctuation = stringAsArray.filter(function checkLetter(letter) {
+        let testLetter = /^[a-z]+$/.test(letter);
+        if(testLetter === true) {
+            return letter;
+        }
+    });
+    
+    let destructArray = arrayNoPunctuation.map(x => x)
+    console.log(destructArray);
+
+    // reverse the string
+    let reverseArray = destructArray.reverse();
+
+    //make array into string
+    reverseArray = reverseArray.join();
+    arrayNoPunctuation = arrayNoPunctuation.join();
+
+    //TEST
+    console.log("stringArray: " + arrayNoPunctuation)
+    console.log("reverse array: " + reverseArray)
+
+    //if string array = reversed string array return true
+    if(arrayNoPunctuation === reverseArray) {
+        return true;
+    }else {
+        return false;
+    }
+    
+    //might have to make the array back to string to compare
+
+    
 
 };
+//TEST
+palindromes("yes");
 
 // Do not edit below this line
 module.exports = palindromes;
